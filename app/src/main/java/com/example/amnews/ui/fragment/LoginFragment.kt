3862c32ba720fb72a9e2123af7d57b618f589930
avelon1A <amanavelon@gmail.com>
2024-02-17 18:55:13 +0530

    dagger hilt base

---
+++ app/src/main/java/com/example/amnews/ui/fragment/LoginFragment.kt
@@ -0,0 +1,57 @@
+package com.example.amnews.ui.fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.amnews.viewModel.LoginViewModel
+import com.example.amnews.databinding.FragmentLoginBinding
+
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: LoginViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentLoginBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.usernameEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            viewModel.login(email,password)
+        }
+        viewModel.result.observe(viewLifecycleOwner){
+            makeToast(it)
+        }
+    }
+
+    private fun makeToast(s: String) {
+        Toast.makeText(requireContext(), s, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
